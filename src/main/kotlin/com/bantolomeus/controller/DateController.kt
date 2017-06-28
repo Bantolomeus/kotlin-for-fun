727799784742598f5a096a7e89a0db759c3888fe
chrb <chrb@spreadshirt.net>
2017-06-28 17:11:12 +0200

    refactored dateController, dunno how it worked before

--- src/main/kotlin/com/bantolomeus/controller/DateController.kt
+++ src/main/kotlin/com/bantolomeus/controller/DateController.kt
@@ -4,16 +4,17 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 @RestController
 class DateController {
 
-    val time: String
         @RequestMapping("/date")
-        get() {
+        fun getDate(): String {
             val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
             val localDateTime = LocalDateTime.now()
+
             return dateTimeFormatter.format(localDateTime)
         }
 
