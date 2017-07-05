f8803c8451d1a78ae1ca6beb93ffb90bc6b2b4f5
Bantolomeus <chrb@spreadshirt.net>
2017-07-05 16:17:07 +0200

    #2 added logic for additional range parameter

--- src/main/kotlin/com/bantolomeus/model/DistributionCreatorModel.kt
+++ src/main/kotlin/com/bantolomeus/model/DistributionCreatorModel.kt
@@ -4,17 +4,30 @@ import java.util.Random
 
 open class DistributionCreatorModel {
 
-    fun createDistribution(range: Int, invocations: Long): Map<Int, Long> {
+    fun createDistribution(range: Int, rangeBegin: Int, rangeEnd: Int, invocations: Long): Map<Int, Long> {
 
-        val distribution = HashMap<Int, Long>()
+        if ((rangeBegin != 0 || rangeEnd != 9) && rangeEnd > rangeBegin) {
+            val rangeCalculated = rangeEnd - rangeBegin
+
+            return getDistributionWithRangeAndInvocations(rangeCalculated, invocations)
+        } else {
+            return getDistributionWithRangeAndInvocations(range, invocations)
+        }
+
+    }
+
+    fun getDistributionWithRangeAndInvocations(range: Int, invocations: Long): HashMap<Int, Long> {
+
+        val distributionToCreate = HashMap<Int, Long>()
         val random = Random()
 
         for (j in 0..invocations - 1) {
 
             val randomNumber = random.nextInt(range)
 
-            distribution.put(randomNumber, (distribution as Map<Int, Long>).getOrDefault(randomNumber, 0L) + 1L)
+            distributionToCreate.put(randomNumber, (distributionToCreate as Map<Int, Long>).getOrDefault(randomNumber, 0L) + 1L)
         }
-        return distribution
+
+        return distributionToCreate
     }
 }
