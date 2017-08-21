4880f2d4fef8495a988c456f6011dde4b16a22ff
Bantolomeus <chrb@spreadshirt.net>
2017-08-21 16:16:00 +0200

    #21 created statistic controller

---
+++ src/main/kotlin/com/bantolomeus/controller/StatisticController.kt
@@ -0,0 +1,23 @@
+package com.bantolomeus.controller
+
+import com.bantolomeus.service.StatisticService
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class StatisticController (private val statisticService: StatisticService) {
+    @RequestMapping(path = arrayOf("/statistic"), method = arrayOf(RequestMethod.GET), produces = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
+    fun getDistribution(
+    ): ResponseEntity<Map<String, Any>> {
+
+        val response = mapOf("normal distribution called" to statisticService.getNormalDistributionCalls(),
+                "uniform distribution called" to statisticService.getUniformDistributionCalls())
+
+        return ResponseEntity(response, HttpStatus.OK)
+    }
+}
