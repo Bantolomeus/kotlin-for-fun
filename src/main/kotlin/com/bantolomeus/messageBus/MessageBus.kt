3bd3fce8bb5be538c47f1f9d84256213e17b2013
Bantolomeus <chrb@spreadshirt.net>
2017-08-22 07:51:14 +0200

    #22 message bus implementation

--- src/main/kotlin/com/bantolomeus/messageBus/MessageBus.kt
+++ src/main/kotlin/com/bantolomeus/messageBus/MessageBus.kt
@@ -1,11 +1,27 @@
 package com.bantolomeus.messageBus
 
+import com.bantolomeus.model.ObserverInterface
+import com.bantolomeus.model.SubscriptionEnum
 import com.bantolomeus.service.StatisticService
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 
-@Component
-class MessageBus (private val statisticService: StatisticService){
+@Service
+// private var observers: Map<SubscriptionEnum, ObserverInterface>,
+class MessageBus(private val statisticService: StatisticService) {
 
+    fun update(message: SubscriptionEnum) {
+        updateObservers(message)
+    }
 
+    fun updateObservers(message: SubscriptionEnum) {
+//        for ((subscription, observer) in observers) {
+            if (message == SubscriptionEnum.NORMAL_DISTRIBUTION) {
+                statisticService.normalDistributionCalled()
+            } else if (message == SubscriptionEnum.UNIFORM_DISTRIBUTION) {
+                statisticService.uniformDistributionCalled()
+            }
+//        }
+    }
 
 }
